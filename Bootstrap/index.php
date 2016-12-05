51492b6b975bdfad4bfa0604d65d7e0d04ac198d
student <student>
2016-12-05 21:25:21 +0200

    Form Boots Plus Html5

---
+++ Bootstrap/index.php
@@ -0,0 +1,7 @@
+<input type="text" required name="name" placeholder="Введите ваше имя"><br>
+    
+    <input type="number" required name="last_name"><br>
+    
+    <input type="date" required name="date"><br>
+    
+    <input type="submit" value="button">
